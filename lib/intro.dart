ba1c364faf496f3b625fd9a14e59e70242cb8f7c
BranchDev98 <artemshykurin98@gmail.com>
2022-02-04 19:49:45 +0200

    webview

--- lib/intro.dart
+++ lib/intro.dart
@@ -9,24 +9,21 @@ class EFTIntroPage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Column(children: <Widget>[
-          Text("hiiiiiiiiiiiii "),
-          Text("hiiiiiiiiiiiii "),
           Container(
-              height: 300,
+              height: MediaQuery.of(context).size.height - 80,
               child: WebView(
                   key: Key("webview1"),
                   debuggingEnabled: true,
                   javascriptMode: JavascriptMode.unrestricted,
                   initialUrl: "https://flutter.dev/")),
-          Text("hiiiiiiiiiiiii "),
-          Text("hiiiiiiiiiiiii "),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Go back!'),
+          )
         ]),
-        /*ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
-        ),*/
+        /*,*/
       ),
     );
   }
